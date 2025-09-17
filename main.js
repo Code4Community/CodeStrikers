fdc328b8dd5f640c44018a4380521cfc139971ed
tejasnaik <naik.tejas11@gmail.com>
2025-09-17 11:15:17 -0400

    added main js

---
+++ main.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", function () {
+  const canvas = this.document.getElementById("canvas1");
+  const ctx = canvas.getContent("2d");
+  canvas.width = 1000;
+  canvas.height = 600;
+});
